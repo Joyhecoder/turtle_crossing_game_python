f5a69b25af692baf66a341f471a849052652daa5
Your GitHub Username <joyhe520@gmail.com>
2024-03-04 14:05:04 -0600

    added logic when the turtle collide with a car

--- main.py
+++ main.py
@@ -21,5 +21,12 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move()
     
+    
+    #Detect collision with a car:
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            
+    
 
 screen.exitonclick()
